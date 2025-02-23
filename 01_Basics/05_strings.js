8bce88c1f80736b16b72b2be7b1515f22f9d1bd2
ankitdhankar07 <ankitdhankar16@gmail.com>
2025-02-23 22:21:14 +0530

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Ankit"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ankit-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('k'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://ankit.com/ankit%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
